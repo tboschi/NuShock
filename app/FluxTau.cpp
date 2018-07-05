214072da4b4a40bfa3f664eb4a32f7f88c71d442
Tommaso.Boschi <tboschi@heppc400.ph.qmul.ac.uk>
2018-07-05 08:21:53 +0100

    05/07/18

--- app/FluxTau.cpp
+++ app/FluxTau.cpp
@@ -84,17 +84,26 @@ int main(int argc, char** argv)
 	TFile *FileOut_ = new TFile(Out_.c_str(), "RECREATE");
 	TFile *FileOutB = new TFile(OutB.c_str(), "RECREATE");
 
-	TH1D * hTotal_ = new TH1D("htotal", "total",  100, 0, 20);
-	TH1D * hTotalB = new TH1D("htotal", "total",  100, 0, 20);
+	TH1D * hTotal_ = new TH1D("htotal1", "total",  100, 0, 20);
+	TH1D * hTotalB = new TH1D("htotal2", "total",  100, 0, 20);
 
 	//neutrino
 	TH1D * hCharm = new TH1D("hcharm", "charm",  100, 0, 20);
 
 	//antineutrino
-	TH1D * hTauE = new TH1D("htaue",  "taue",   100, 0, 20);
+	TH1D * hTauE  = new TH1D("htaue",  "taue",   100, 0, 20);
 	TH1D * hTauM  = new TH1D("htaum",  "taum",   100, 0, 20);
 	TH1D * hPion  = new TH1D("hpion",  "pion",   100, 0, 20);
 	TH1D * h2Pion = new TH1D("h2pion", "2 pion", 100, 0, 20);
+
+	//hTotal_->SetDirectory(FileOut_);
+	hCharm->SetDirectory(FileOut_);
+
+	//hTotalB->SetDirectory(FileOutB);
+	hTauE->SetDirectory(FileOutB);
+	hTauM->SetDirectory(FileOutB);
+	hPion->SetDirectory(FileOutB);
+	h2Pion->SetDirectory(FileOutB);
 
 	//generous angular acceptance of detector
 	Detector *TheBox = new Detector(DetConfig);
@@ -115,18 +124,22 @@ int main(int argc, char** argv)
 	double maxF = lDparam(0, 0);
 	double minF = lDparam(1, ptmax);
 
-	std::ofstream Dist("distribution.dat");
-	std::vector<Particle*> vProducts;
+	double SF = 12.1e-3 / 331.4 * 0.077 * 0.0548 / double(nMAX);
+
+	std::vector<Particle*> vProductDs, vProductTau;
+	std::vector<Particle*>::iterator iP;
+
 	unsigned int nIter = 0, iIter = 0;
 	while (iIter < nMAX)
 	{
+		for (iP = vProductDs.begin(); iP != vProductDs.end(
+		vProductDs.clear();k
+
 		++nIter;
-		vProducts.clear();
 		double pt = Gen->Uniform(0, ptmax);
 		double xf = Gen->Uniform(-1.0, 1.0);
 		if (Gen->Uniform(0, pow(10, maxF)) < pow(10, lDparam(xf, pt)))
 		{
-			Dist << xf << "\t" << pt << "\t" << lDparam(xf, pt) << std::endl;
 			double px, py;
 			double pz = sqrts*xf*0.5;
 			Gen->Circle(px, py, pt);
@@ -140,7 +153,7 @@ int main(int argc, char** argv)
 				continue;
 
 			if (vProducts.at(0)->Theta() < Th0)	//neutrino
-				hCharm->Fill(vProducts.at(0)->Energy());
+				hCharm->Fill(vProducts.at(0)->Energy(), SF);
 
 			TLorentzVector Tau_vec = vProducts.at(1)->FourVector();
 
@@ -148,23 +161,28 @@ int main(int argc, char** argv)
 			{
 				Amplitude::Channel Name;
 				TH1D* hFill;
+				double Br;
 				switch (i)
 				{
 					case 0:
 						Name = Amplitude::_TauET;
 						hFill = hTauE;
+						Br = SF * 0.1785;
 						break;
 					case 1:
 						Name = Amplitude::_TauMT;
 						hFill = hTauM;
+						Br = SF * 0.1736;
 						break;
 					case 2:
 						Name = Amplitude::_TauPI;
 						hFill = hPion;
+                                                Br = SF * 0.1082;
 						break;
 					case 3:
 						Name = Amplitude::_Tau2PI;
 						hFill = h2Pion;
+                                                Br = SF * 0.2551;
 						break;
 					default:
 						break;
@@ -174,20 +192,26 @@ int main(int argc, char** argv)
 				vProducts = NuB->ProductionPS(Name, Tau_vec);
 
 				if (vProducts.at(0)->Theta() < Th0)	//neutrino
-					hFill->Fill(vProducts.at(0)->Energy());
+					hFill->Fill(vProducts.at(0)->Energy(), Br);
 			}
 
 			++iIter;
+
+			if (iIter % 100000 == 0)
+			{
+				FileOut_->Write();
+				FileOutB->Write();
+			}
 		}
 	}
 	//		cc	pC	fDs	Br	Tot evts  
-	double SF = 12.1e-3 / 331.4 * 0.077 * 0.0548 / double(nMAX);
+	//double SF = 12.1e-3 / 331.4 * 0.077 * 0.0548 / double(nMAX);
 
-	hCharm->Scale(SF);
-	hTauE->Scale(SF * 0.1785);	//tau->e (17.85 %)
-	hTauM->Scale(SF * 0.1736);	//tau->mu (17.36 %)
-	hPion->Scale(SF * 0.1082);	//tau->pi (10.82 %)
-	h2Pion->Scale(SF * 0.2551);	//tau->2pi (25.62 %)	Phase space only!!
+	//hCharm->Scale(SF);
+	//hTauE->Scale(SF * 0.1785);	//tau->e (17.85 %)
+	//hTauM->Scale(SF * 0.1736);	//tau->mu (17.36 %)
+	//hPion->Scale(SF * 0.1082);	//tau->pi (10.82 %)
+	//h2Pion->Scale(SF * 0.2551);	//tau->2pi (25.62 %)	Phase space only!!
 
 	hTotal_->Add(hCharm);
 
@@ -197,17 +221,19 @@ int main(int argc, char** argv)
 	hTotalB->Add(h2Pion);
 
 	FileOut_->cd();
+	FileOut_->Write();
 
-	hTotal_->Write();
-	hCharm->Write();
+	hTotal_->Write("htotal");
+	//hCharm->Write();
 
 	FileOutB->cd();
+	FileOutB->Write();
 
-	hTotalB->Write();
-	hTauE->Write();
-	hTauM->Write();
-	hPion->Write();
-	h2Pion->Write();
+	hTotalB->Write("htotal");
+	//hTauE->Write();
+	//hTauM->Write();
+	//hPion->Write();
+	//h2Pion->Write();
 
 	std::cout << "Simulated Ds meson producing succesful neutrinos are " << (100.0 * iIter) / nIter << " % of the total" << std::endl;
 	std::cout << hTotal_->GetEntries() << " neutrinos simulated, saved in ";
